aa3b8bcd5cf96bf58b1fb376a3b883bd920c5932
Otavio <estudosprogotavio@gmail.com>
2025-04-07 06:45:13 -0300

    Adicionando o repositorio da entidade endereco

---
+++ src/main/java/dev/Zerphyis/listagem/Repository/AddresRepository.java
@@ -0,0 +1,7 @@
+package dev.Zerphyis.listagem.Repository;
+
+import dev.Zerphyis.listagem.Entits.Addres.Addres;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddresRepository extends JpaRepository<Addres,Long> {
+}
