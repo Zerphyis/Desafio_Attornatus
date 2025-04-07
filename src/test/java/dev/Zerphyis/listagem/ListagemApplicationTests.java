7c2dad4ca78a6e2d33c9493898ecd38542a80aae
Otavio <estudosprogotavio@gmail.com>
2025-04-07 06:41:32 -0300

    Commit inicial

---
+++ src/test/java/dev/Zerphyis/listagem/ListagemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.Zerphyis.listagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListagemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
