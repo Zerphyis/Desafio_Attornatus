548e263a0fa35d5b7fa0856bd34dffff2546e666
Otavio <estudosprogotavio@gmail.com>
2025-04-07 06:45:22 -0300

    Adicionando a service da entidade endereco

---
+++ src/main/java/dev/Zerphyis/listagem/Service/AddresService.java
@@ -0,0 +1,46 @@
+package dev.Zerphyis.listagem.Service;
+
+import dev.Zerphyis.listagem.Entits.Addres.Addres;
+import dev.Zerphyis.listagem.Entits.Person.Person;
+import dev.Zerphyis.listagem.Exeception.AddresNotFoundExeception;
+import dev.Zerphyis.listagem.Exeception.PersonNotFoundExeception;
+import dev.Zerphyis.listagem.Records.DataAddres;
+import dev.Zerphyis.listagem.Repository.AddresRepository;
+import dev.Zerphyis.listagem.Repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AddresService {
+    @Autowired
+    PersonRepository personRepository;
+    @Autowired
+    AddresRepository addresRepository;
+
+    public Addres create(Long personId, DataAddres data) {
+        Person person = personRepository.findById(personId)
+                .orElseThrow(() -> new PersonNotFoundExeception("Pessoa não encontrada com ID: " + personId));
+
+        // Desativa endereço principal anterior, se houver
+        person.getEnderecos().forEach(Addres::desativeMain);
+
+        Addres addres = new Addres(data, person);
+        return addresRepository.save(addres);
+    }
+
+    public List<Addres> listAll() {
+        return addresRepository.findAll();
+    }
+
+    public Addres getById(Long id) {
+        return addresRepository.findById(id)
+                .orElseThrow(() -> new AddresNotFoundExeception("Endereço não encontrado com ID: " + id));
+    }
+
+    public void delete(Long id) {
+        Addres address = getById(id);
+        addresRepository.delete(address);
+    }
+}
